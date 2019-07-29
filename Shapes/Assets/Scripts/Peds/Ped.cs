f1a531f7ef33a4fda977c400d1ca6256e0254ffc
Joe Davis <joe--davis@hotmail.com>
2019-07-30 00:08:27 +0100

    v1.1 - More Player Anim
    - Plays idle, walkLeft and walkkRight anims.

--- Shapes/Assets/Scripts/Peds/Ped.cs
+++ Shapes/Assets/Scripts/Peds/Ped.cs
@@ -4,9 +4,53 @@ using UnityEngine;
 
 public class Ped : MonoBehaviour
 {
-	// Start is called before the first frame update
-	void Start()
-	{
 
+	private string pedName;
+	private string sound;
+	private float acceleration;
+	private float maxSpeed;
+
+	// Ped Name
+	public void setName(string newName)
+	{
+		pedName = newName;
+	}
+
+	public string getName()
+	{
+		return pedName; 
+	}
+
+	// Ped Sound
+	public void setSound(string newSound)
+	{
+		sound = newSound;
+	}
+
+	public string getSound()
+	{
+		return sound; 
+	}
+
+	// Ped Movement Speed
+	public void setAcceleration(float newAcceleration)
+	{
+		acceleration = newAcceleration;
+	}
+
+	public float getAcceleration()
+	{
+		return acceleration; 
+	}
+
+	// Ped Max Speed
+	public void setMaxSpeed(float newMaxSpeed)
+	{
+		maxSpeed = newMaxSpeed;
+	}
+
+	public float getMaxSpeed()
+	{
+		return maxSpeed; 
 	}
 }
---
+++ Shapes/Assets/Scripts/Peds/Player.cs
@@ -0,0 +1,79 @@
+﻿/* 
+* Author: Joe Davis
+* Project: Shapes
+* 2019
+* Notes: 
+* This is used to control the player.
+* Attach this to the player GameObject,
+* WORK IN PROGRESS
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Ped
+{
+	// Components
+	private Rigidbody2D rb2d;
+	private Collider2D Collider2D;
+	private Animator anim;
+
+	// Global Variables
+	[Header("Player Settings")]
+	[SerializeField][Range(0.1f, 10.0f)]
+	private float _acceleration = 0.1f;
+	[SerializeField][Range(0.1f, 10.0f)]
+	private float _maxSpeed = 0.1f;
+
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		setName("Morphy");
+		setAcceleration(_acceleration);
+		setMaxSpeed(_maxSpeed);
+	}
+
+	private void Start()
+	{
+		Collider2D = GetComponent<Collider2D>();
+		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		MovePlayer();
+	}
+
+	private void FixedUpdate()
+	{
+
+	}
+
+	private void MovePlayer()
+	{
+		float moveHorizontal;
+
+		rb2d.bodyType = RigidbodyType2D.Dynamic;
+		//grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+		moveHorizontal = Input.GetAxisRaw("Horizontal");
+		Vector2 movement = new Vector2 (moveHorizontal, 0);
+		if((rb2d.velocity.x >= -_maxSpeed && rb2d.velocity.x <= _maxSpeed)){
+			rb2d.AddForce(movement * _acceleration);
+		}
+		if(rb2d.velocity.x > 0)
+		{
+			anim.Play("WalkRight");
+		}
+		else if(rb2d.velocity.x < 0)
+		{
+			anim.Play("WalkLeft");
+		}
+		else{
+			anim.Play("Idle");
+		}
+	}
+}
